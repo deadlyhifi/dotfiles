828d920e8fa7fd01b1b65c4a7ff354a790873a44
Tom de Bruin <tom@deadlyhifi.com>
2016-10-22 04:41:41 +0100

    Make it mine
    Further macOS default tweaks

--- install.sh
+++ install.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
-echo "Setting up your Mac..."
-
+echo "Setting up your Mac…"
+#
 # Check for Homebrew and install if we don't have it
 if test ! $(which brew); then
   ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
@@ -22,12 +22,21 @@ curl -sS https://getcomposer.org/installer | php
 mv composer.phar /usr/local/bin/composer
 
 # Install global Composer packages
-/usr/local/bin/composer global require laravel/installer laravel/lumen-installer tightenco/jigsaw
+# /usr/local/bin/composer global require laravel/installer laravel/lumen-installer tightenco/jigsaw
 
-# Create a Sites directory
-# This is a default directory for OS X user accounts but doesn't comes pre-installed
-mkdir $HOME/Sites
+# Create symlinks
+ln -s ~/Dropbox/Code ~/Code
+rm ~/.zshrc
+ln -s ~/Dropbox/AppSync/_Symlinks/Shell/zshrc ~/.zshrc
+rm -r ~/.atom
+ln -s ~/Dropbox/AppSync/_Symlinks/dotAtom ~/.atom
+rm ~/.gitignore_global
+ln -s ~/Dropbox/AppSync/_Symlinks/git/gitignore_global ~/.gitignore_global
+rm -r ~/.homestead
+ln -s ~/Dropbox/AppSync/_Symlinks/dotHomestead ~/.homestead
+rm -r .oh-my-zsh
+ln -s ~/Dropbox/AppSync/_Symlinks/dotOh-my-zsh .oh-my-zsh
 
 # Set OS X preferences
 # We will run this last because this will reload the shell
-source .osx
+source .macOS
---
+++ manual.md
@@ -0,0 +1,24 @@
+# Applications
+
+Most applications get installed through Homebrew Cask. The apps below need to be installed manually, through the AppStore unless otherwise stated.
+
+- pixelmator
+- todoist
+- tweetbot
+- hazel - https://www.noodlesoft.com/Products/Hazel/download
+- scidaconverter - http://getscida.com/convert/
+- npm - https://nodejs.org/en/
+- nvm - `curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.32.1/install.sh | bash`, `nvm install node` [github.com/creationix/nvm](https://github.com/creationix/nvm)
+
+# Set up
+
+## iTerm
+
+Preferences:
+Colors [sic], color presets -> import -> ~/Dropbox/AppSync/\_Symlinks/dotOh-my-zsh/custom/cobalt2.itermcolors
+Text: Change Font -> Fira Code 14
+
+
+Figure out: https://deadlyhifi.com/2016/02/super-duper-power-key-change-capslock-you-dont-use-it-anyway/ for Sierra
+
+When done with copying files and set up, [Inherit BackBlaze backup state](https://help.backblaze.com/hc/en-us/articles/217666358-Inherit-Backup-State).
