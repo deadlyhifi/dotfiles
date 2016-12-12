c0ce608b87238dd87e03e4b4ee5cc0b74f9a6970
Tom de Bruin <tom@deadlyhifi.com>
2016-12-12 20:40:04 +0000

    Install .oh-my-zsh

--- install.sh
+++ install.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 echo "Setting up your Mac…"
-#
+
 # Check for Homebrew and install if we don't have it
 if test ! $(which brew); then
   ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
@@ -13,6 +13,9 @@ brew update
 # Install all our dependencies with bundle (See Brewfile)
 brew tap homebrew/bundle
 brew bundle
+
+# Install oh-my-zsh
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
 
 # Make ZSH the default shell environment
 chsh -s $(which zsh)
@@ -26,16 +29,21 @@ mv composer.phar /usr/local/bin/composer
 
 # Create symlinks
 ln -s ~/Dropbox/Code ~/Code
+
 rm ~/.zshrc
 ln -s ~/Dropbox/AppSync/_Symlinks/Shell/zshrc ~/.zshrc
-rm -r ~/.atom
-ln -s ~/Dropbox/AppSync/_Symlinks/dotAtom ~/.atom
-rm ~/.gitignore_global
-ln -s ~/Dropbox/AppSync/_Symlinks/git/gitignore_global ~/.gitignore_global
-rm -r ~/.homestead
-ln -s ~/Dropbox/AppSync/_Symlinks/dotHomestead ~/.homestead
+
 rm -r .oh-my-zsh
 ln -s ~/Dropbox/AppSync/_Symlinks/dotOh-my-zsh .oh-my-zsh
+
+rm -r ~/.atom
+ln -s ~/Dropbox/AppSync/_Symlinks/dotAtom ~/.atom
+
+rm ~/.gitignore_global
+ln -s ~/Dropbox/AppSync/_Symlinks/git/gitignore_global ~/.gitignore_global
+
+rm -r ~/.homestead
+ln -s ~/Dropbox/AppSync/_Symlinks/dotHomestead ~/.homestead
 
 # Set OS X preferences
 # We will run this last because this will reload the shell
