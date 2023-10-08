14538c23f9db4e990ff27897ca0f0065c3d73d16
Tom de Bruin <122679+deadlyhifi@users.noreply.github.com>
2023-10-08 13:44:45 +0100

    Update symlinks.sh

--- symlinks.sh
+++ symlinks.sh
@@ -6,13 +6,13 @@ git config --global core.excludesfile ~/.gitignore_global
 
 # Shell
 rm ~/.zshrc
-ln -s ~/Google\ Drive/AppSync/_Symlinks/shell/dotZshrc ~/.zshrc
+ln -s ~/Library/CloudStorage/GoogleDrive-tomdebruin@gmail.com/My\ Drive/AppSync/_Symlinks/shell/dotZshrc ~/.zshrc
 rm -r ~/.oh-my-zsh
 ln -s ~/Google\ Drive/AppSync/_Symlinks/dotOh-my-zsh ~/.oh-my-zsh
 
 # Hyper.js
 rm -r ~/.hyper.js
-ln -s ~/Google\ Drive/AppSync/_Symlinks/dotHyper.js ~/.hyper.js
+ln -s ~/Library/CloudStorage/GoogleDrive-tomdebruin@gmail.com/My\ Drive/AppSync/_Symlinks/dotHyper.js ~/.hyper.js
 rm -r ~/.hyper_plugins
 ln -s ~/Library/CloudStorage/GoogleDrive-tomdebruin@gmail.com/My\ Drive/AppSync/_Symlinks/dotHyper_plugins ~/.hyper_plugins
 
