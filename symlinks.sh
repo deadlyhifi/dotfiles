86020158159282f2190b7963cb8feb5d31284a88
Tom de Bruin <122679+deadlyhifi@users.noreply.github.com>
2023-10-08 13:31:26 +0100

    Update Drive path

--- symlinks.sh
+++ symlinks.sh
@@ -1,7 +1,7 @@
 # Set up symlinks for settings
 # Git
 rm ~/.gitignore_global
-ln -s ~/Google\ Drive/AppSync/_Symlinks/git/dotGitignore_global ~/.gitignore_global
+ln -s ~/Library/CloudStorage/GoogleDrive-tomdebruin@gmail.com/My\ Drive/AppSync/_Symlinks/git/dotGitignore_global ~/.gitignore_global
 git config --global core.excludesfile ~/.gitignore_global
 
 # Shell
@@ -14,8 +14,8 @@ ln -s ~/Google\ Drive/AppSync/_Symlinks/dotOh-my-zsh ~/.oh-my-zsh
 rm -r ~/.hyper.js
 ln -s ~/Google\ Drive/AppSync/_Symlinks/dotHyper.js ~/.hyper.js
 rm -r ~/.hyper_plugins
-ln -s ~/Google\ Drive/AppSync/_Symlinks/dotHyper_plugins ~/.hyper_plugins
+ln -s ~/Library/CloudStorage/GoogleDrive-tomdebruin@gmail.com/My\ Drive/AppSync/_Symlinks/dotHyper_plugins ~/.hyper_plugins
 
 # Karabiner
 rm -r ~/.config/karabiner
-ln -s ~/Google\ Drive/AppSync/_Symlinks/dotKarabiner ~/.config/karabiner
+ln -s ~/Library/CloudStorage/GoogleDrive-tomdebruin@gmail.com/My\ Drive/AppSync/_Symlinks/dotKarabiner ~/.config/karabiner
